55018199edef038af8a22be9fe55556d33d390b7
Aniket Raj <92985056+Aniketraj19@users.noreply.github.com>
2023-08-10 12:29:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/header"; // Correct the import path and capitalize the component name
+import Home from "./Components/home";
+
+function App() {
+  return (
+    <>
+      <Header /> {/* Use the Header component */}
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Header.css";
+
+function header() {
+  return <nav>Get ready to change you daily goals</nav>;
+}
+export default header;
